e2c064f4133c67bed68aa6a6f7f9a3d8826b9f17
Haira Lei <darhaii13@gmail.com>
2022-08-04 15:27:55 +0800

    Change variable name of default img

--- src/Movies.js
+++ src/Movies.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useGlobalContext } from "./context";
 import { Link } from "react-router-dom";
 
-const url =
+const defaultImg =
   "https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png";
 
 const Movies = () => {
@@ -20,7 +20,7 @@ const Movies = () => {
         return (
           <Link key={id} to={`/movies/${id}`} className="movie">
             <article>
-              <img src={poster === "N/A" ? url : poster} alt={title} />
+              <img src={poster === "N/A" ? defaultImg : poster} alt={title} />
               <div className="movie-info">
                 <h4 className="title">{title}</h4>
                 <p>{year}</p>
